a880068a2929f83028dce2781ff9a1877ee1f495
Arman Kiran <77576380+armankiran@users.noreply.github.com>
2022-05-22 12:51:20 +0100

    Update sniffer.py

--- sniffer.py
+++ sniffer.py
@@ -1,41 +1,25 @@
 import os
-import sys
 import subprocess
+import argparse
 from geopy.geocoders import Nominatim
 
-#img = sys.argv[1]
-
-
-    # while True:
-    #     img = sys.argv[1]
-    #     if img == 'quit': break
-#sys.argv[1]
-
 def main():
-    img = sys.argv[1]
-    if img == '-h':
-        print('-a = scan all entries\n-d, -d PATH: show the image files in the directory')
-    elif img == '-a':
-        if len(sys.argv) < 3:
-            check_all(os.getcwd())
-        else:
-            try:
-                check_all(sys.argv[2])
-            except FileNotFoundError:
-                print('Please enter a valid path.\nType -h for help')
-    elif img == '-d':
-        if len(sys.argv) < 3:
-            check_dir(os.getcwd())
-        else:
-            try:
-                check_dir(sys.argv[2])
-            except FileNotFoundError:
-                print('Please enter a valid path.\nType -h for help')
-    else:
-        check_single(img)
+    #TODO add option to scan multiple images
+    parser = argparse.ArgumentParser(description='A test program.')
+    parser.add_argument("-a", "--all", nargs="?", const=os.getcwd(), help="Scans all.")
+    parser.add_argument("-s", "--single", help="Scans an image.")
+    parser.add_argument("-l", "--list", nargs="?", const=os.getcwd(), help="Lists images.")
+    args = parser.parse_args()
+    if args.all:
+        check_all(args.all)
+    elif args.single:
+        check_single(args.single)
+    elif args.list:
+        check_dir(args.list)
 
 
 def check_single(img):
+    '''get exif data and location from the image'''
     try:
         gps = get_exif(img)
     except:
@@ -45,6 +29,8 @@ def check_single(img):
     print(location)
 
 def check_all(dir):
+    '''scan all the images from the directory'''
+    #TODO add more file formats
     with os.scandir(dir) as dir:
         for entry in dir:
             if not entry.name.startswith('.') and entry.is_file() and entry.name.lower().endswith(('.jpg', '.jpeg', '.png')):
@@ -52,6 +38,8 @@ def check_all(dir):
                 check_single(entry.name)
 
 def check_dir(dir):
+    '''get lists of images from the directory'''
+    #TODO add more file formats
     with os.scandir(dir) as dir:
         for entry in dir:
             if not entry.name.startswith('.') and entry.is_file() and entry.name.lower().endswith(('.jpg', '.jpeg', '.png')):
